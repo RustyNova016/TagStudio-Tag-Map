015f0373471835ecb3724546b7813d80fb763141
RustyNova <rusty.nova.jsb@gmail.com>
2026-05-07 17:29:24 +0200

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Basic flake for a Bun.js project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bun
+            nodejs
+          ];
+
+          shellHook = ''
+          '';
+        };
+      }
+    );
+}
